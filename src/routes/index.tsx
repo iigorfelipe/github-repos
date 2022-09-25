2477e3be1dc51da23fbebf9357c85e6b16cfca2d
Igor <iigorfelipe@gmail.com>
2022-09-25 20:39:41 -0300

    ✨ feat: implements routes

---
+++ src/routes/index.tsx
@@ -0,0 +1,16 @@
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import SearchUser from '../pages/SearchUser';
+import Main from '../pages/Main';
+
+const AppRoutes = () => (
+  <BrowserRouter>
+    <Routes>
+  
+      <Route path='/' element={ <SearchUser /> } />
+      <Route path='/repository' element={ <Main /> } />
+
+    </Routes>
+  </BrowserRouter>
+);
+
+export default AppRoutes;
