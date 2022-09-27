5405e9a92621e7bc5b634b7dacae31ad3f39e4a2
Igor <iigorfelipe@gmail.com>
2022-09-27 14:36:41 -0300

    ✨ feat: add style and theme in application

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,36 @@
-import AppRoutes from './routes';
+/* eslint-disable prettier/prettier */
+import { DefaultTheme, ThemeProvider } from 'styled-components';
+import { Dark, Ligth, GlobalStyle } from './styles';
+
 import GithubProvider from './providers/GithubProvider';
 
-const App = () => (
-  <GithubProvider>
-    <AppRoutes />
-  </GithubProvider>
-);
+import usePersistedState from './helpers/usePersistedState';
+
+import AppRoutes from './routes';
+import Header from './components/Header';
+
+const App = () => {
+  const [theme, setTheme] = usePersistedState<DefaultTheme>('theme', Dark);
+
+  const toggleTheme = () => {
+    setTheme(theme.title === 'ligth' ? Dark : Ligth);
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+
+      <GlobalStyle />
+
+      <GithubProvider>
+
+        <Header toggleTheme={toggleTheme} />
+
+        <AppRoutes />
+
+      </GithubProvider>
+
+    </ThemeProvider>
+  );
+};
 
 export default App;
