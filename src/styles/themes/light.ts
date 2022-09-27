37fa77a84b5387b64801dd966f68eef19e975952
Igor <iigorfelipe@gmail.com>
2022-09-27 14:33:20 -0300

    ✨ feat: create Ligth theme

---
+++ src/styles/themes/light.ts
@@ -0,0 +1,16 @@
+const Ligth = {
+  title: 'ligth',
+
+  background: {
+    primary: '#fff',
+    secundary: '#f6f8fa',
+  },
+
+  colors: {
+    primary: '#24292f',
+    secundary: '#57606a',
+    link: '#0969da',
+  },
+};
+
+export default Ligth;
