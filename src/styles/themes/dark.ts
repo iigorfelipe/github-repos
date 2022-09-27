001b9f6506205a97d0024d2bb859a6eae106ac5d
Igor <iigorfelipe@gmail.com>
2022-09-27 14:33:06 -0300

    ✨ feat: create Dark theme

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,16 @@
+const Dark = {
+  title: 'dark',
+
+  background: {
+    primary: '#0c1017',
+    secundary: '#151a22',
+  },
+
+  colors: {
+    primary: '#c9d1d9',
+    secundary: '#8b949e',
+    link: '#58a6ff',
+  },
+};
+
+export default Dark;
