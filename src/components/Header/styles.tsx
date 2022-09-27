57c6a46155147e8304bbcb1a54a7ef0e07348b19
Igor <iigorfelipe@gmail.com>
2022-09-27 20:28:45 -0300

    🎨 style: implement styling to the Header component

---
+++ src/components/Header/styles.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  height: 60px;
+  color: #fff;
+  background: #24292f;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 30px;
+`;
+
+export default Container;
