d077e7f445e18220dc5ac2df15479eb80c254564
Igor <iigorfelipe@gmail.com>
2022-09-29 09:07:13 -0300

    🎨 style: addidioan spacing on the Home page

--- src/pages/Home/styles.tsx
+++ src/pages/Home/styles.tsx
@@ -2,6 +2,9 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   display: flex;
+  justify-content: space-between;
+  padding: 0px 32px;
+  margin: 0px 35px;
 `;
 
 export const Message = styled.div`
