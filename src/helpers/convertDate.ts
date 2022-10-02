0d817a34dc8e2b80f3e4c67a262910ecf36f27a9
Igor <iigorfelipe@gmail.com>
2022-10-02 15:40:17 -0300

    ✨ feat: implements getDayAndMonth

---
+++ src/helpers/convertDate.ts
@@ -0,0 +1,11 @@
+const getDayAndMouth = (dayAndMouth: string) => {
+  const date = new Date(`${dayAndMouth}`);
+
+  const day = date.toLocaleString('en-US', { day: 'numeric' });
+
+  const month = date.toLocaleString('en-US', { month: 'short' });
+
+  return `${day} ${month}`;
+};
+
+export default getDayAndMouth;
