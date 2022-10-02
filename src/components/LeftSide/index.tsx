09b4da41adbdba832cb46aad7ef6fe33b024216d
Igor <iigorfelipe@gmail.com>
2022-10-02 13:05:38 -0300

    ✨ feat: render information only if it exists

--- src/components/LeftSide/index.tsx
+++ src/components/LeftSide/index.tsx
@@ -12,13 +12,19 @@ const LeftSide = () => {
 
   return (
     <C.LeftSide>
+
       <C.Avatar src={infos.avatar_url} alt="" />
+
       <C.Name>{infos.name}</C.Name>
+
       <C.Login>{infos.login}</C.Login>
+
       <C.BtnFollow>Follow</C.BtnFollow>
+
       <C.Bio>{infos.bio}</C.Bio>
 
       <C.FollowersDiv>
+
         <C.Image src={`./users-bold-${title}.svg`} />
 
         <C.Links>
@@ -30,31 +36,59 @@ const LeftSide = () => {
         <C.Links>
           <C.Numbers>{infos.following}</C.Numbers>following
         </C.Links>
+
       </C.FollowersDiv>
 
       <C.Div>
-        <C.Div2>
-          <C.Image src={`./building-${title}.svg`} />
-          <C.Company>{infos.company}</C.Company>
-        </C.Div2>
 
-        <C.Div2>
-          <C.Image src={`./link-${title}.svg`} />
-          <C.Location>{infos.location}</C.Location>
-        </C.Div2>
+        {
+          infos.company &&
+            <C.Div2>
 
-        <C.Div2>
-          <C.Image src={`./map-pin-${title}.svg`} />
-          <C.Blog>{infos.blog}</C.Blog>
-        </C.Div2>
+              <C.Image src={`./building-${title}.svg`} />
 
-        <C.Div2>
-          <C.Image src={`./twitter-fill-${title}.svg`} />
-          <C.Twitter>{infos.twitter_username}</C.Twitter>
-        </C.Div2>
+              <C.Company>{infos.company}</C.Company>
+
+            </C.Div2>
+        }
+
+        {
+          infos.location &&
+            <C.Div2>
+
+              <C.Image src={`./link-${title}.svg`} />
+
+              <C.Location>{infos.location}</C.Location>
+
+            </C.Div2>
+        }
+
+        {
+          infos.blog &&
+            <C.Div2>
+
+              <C.Image src={`./map-pin-${title}.svg`} />
+
+              <C.Blog>{infos.blog}</C.Blog>
+
+            </C.Div2>
+        }
+
+        {
+          infos.twitter_username && 
+            <C.Div2>
+
+              <C.Image src={`./twitter-fill-${title}.svg`} />
+
+              <C.Twitter>{infos.twitter_username}</C.Twitter>
+
+            </C.Div2>
+        }
+
       </C.Div>
 
       <C.Line />
+
     </C.LeftSide>
   );
 };
