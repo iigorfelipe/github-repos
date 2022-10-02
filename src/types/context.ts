78d6c95ef50e1624bfec00d7fab42a9525c5999a
Igor <iigorfelipe@gmail.com>
2022-10-01 21:49:29 -0300

    ✨ feat: add type to reposFiltered

--- src/types/context.ts
+++ src/types/context.ts
@@ -1,8 +1,10 @@
-import { UserInfos } from './user';
+import { Repo, UserInfos } from './user';
 
 export type UserContextType = {
   searchInputValue: string;
   setSearchInputValue: (newState: string) => void;
   userInformation: UserInfos;
   setUserInformation: (newState: UserInfos) => void;
+  reposFiltered: Repo[];
+  setReposFiltered: (newState: Repo[]) => void;
 };
