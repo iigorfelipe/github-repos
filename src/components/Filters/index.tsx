99b3bf9ea47197d8971975b6c1ef7d0882544c7b
Igor <iigorfelipe@gmail.com>
2022-10-01 21:53:45 -0300

    ✨ feat: filters the repositories the user types

--- src/components/Filters/index.tsx
+++ src/components/Filters/index.tsx
@@ -1,14 +1,29 @@
+import { useContext, useEffect, useState } from 'react';
+import GithubContext from '../../providers/GithubContext';
 import * as C from './styles';
 
 const Filters = () => {
+  const {
+    userInformation: { repos },
+    setReposFiltered,
+  } = useContext(GithubContext);
+  const [searchRepo, setSearchRepo] = useState('');
+
+  useEffect(() => {
+    const filtered = repos.filter((repo) => repo.name?.startsWith(searchRepo));
+
+    setReposFiltered(filtered);
+  }, [repos, searchRepo, setReposFiltered]);
+
   return (
     <C.Div>
       <C.Input
         type="text"
         placeholder="Encontre um repositório"
-        // value={}
-        // onChange={({ target: { value } }) => (value)}
+        value={searchRepo}
+        onChange={({ target: { value } }) => setSearchRepo(value)}
       />
+
       <C.Select>
         <C.Option>Tipo</C.Option>
       </C.Select>
