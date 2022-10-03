0d220fd40e76ba36a96ab6b8e2da6709c63f2939
Igor <iigorfelipe@gmail.com>
2022-10-02 22:48:51 -0300

    ✨ feat: add link and linkHover type

--- src/styles/styled.d.ts
+++ src/styles/styled.d.ts
@@ -8,6 +8,8 @@ declare module 'styled-components' {
       primary: string;
       secundary: string;
       tertiary?: string;
+      link: string;
+      linkHover: string;
     };
 
     colors: {
