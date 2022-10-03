0046b5b434763b45b407c9c68344c14ff68dfbcb
Igor <iigorfelipe@gmail.com>
2022-10-03 16:32:48 -0300

    ✨ feat: implements Carousel component

--- src/components/Display/Carousel/index.tsx
+++ src/components/Display/Carousel/index.tsx
@@ -1,7 +1,145 @@
+import Carousel from 'nuka-carousel/lib/carousel';
+import { useContext } from 'react';
+
+import { ThemeContext } from 'styled-components';
+import GithubContext from '../../../providers/GithubContext';
+
+import languages from '../../../helpers/languageColors.json';
+import getDayAndMouth from '../../../helpers/convertDate';
+
 import * as C from './styles';
 
-const Carousel = () => {
-  return <C.Container>CAROUSEL</C.Container>;
+const CarouselDisplay = () => {
+  const { title } = useContext(ThemeContext);
+
+  const { reposFiltered } = useContext(GithubContext);
+
+  return (
+    <Carousel
+      wrapAround
+      withoutControls
+      cellSpacing={20}
+      slidesToShow={2.5}
+    >
+      {
+        reposFiltered.map((repo) => (
+          <C.Container key={repo.id}>
+
+            <C.DivTitle>
+
+              <C.Title>{repo.name}</C.Title>
+
+              <C.Visibility>{repo.visibility}</C.Visibility>
+
+            </C.DivTitle>
+
+            <C.Div>
+
+              <C.Descrition>
+                {
+                  repo.description
+                    ? repo.description
+                    : 'Descrição vazia.'
+                }
+              </C.Descrition>
+
+            </C.Div>
+
+            {
+              repo.language &&
+                <C.DivLanguage>
+
+                  <C.Language languageColor={languages[repo.language]} />
+
+                  <C.Span>{repo.language}</C.Span>
+
+                </C.DivLanguage>
+            }
+
+            <C.DivTopics>
+
+              {
+                repo.topics?.length
+                  ? repo.topics?.length > 0 &&
+                    <C.DivTopics>
+
+                      {          
+                        repo.topics?.map((topic) => (
+                          <C.Topics key={repo.id}>{topic}</C.Topics>
+                        ))
+                      }
+
+                    </C.DivTopics>
+                  : ''
+              }
+
+            </C.DivTopics>
+
+            <C.Details>
+
+              {
+                repo.stargazers_count > 0 &&
+                  <C.DetailStar>
+
+                    <C.Image src={`./star-${title}.svg`} />
+
+                    <C.Span>{repo.stargazers_count}</C.Span>
+
+                  </C.DetailStar>
+              }
+
+              {
+                repo.forks
+                  ? repo.forks >= 1 &&
+                    <C.DetailFork>
+
+                      <C.Image src={`./repo-forked-16-${title}.svg`} />
+
+                      <C.Span>{repo.forks}</C.Span>
+
+                    </C.DetailFork>
+                  : ''
+              }
+
+              {
+                repo.license && 
+                  <C.Detail>
+                    <C.Image src={`./balance-scale-${title}.svg`} />
+
+                    <C.Span>{repo.license.name}</C.Span>
+
+                  </C.Detail>
+              }
+
+            </C.Details>
+
+            <C.Details>
+
+              {
+                repo.created_at && 
+                  <C.Detail>
+
+                    <C.Span>Created on {getDayAndMouth(repo.created_at)}</C.Span>
+
+                  </C.Detail>
+              }
+
+              {
+                repo.updated_at &&
+                  <C.Detail>
+
+                    <C.Span>Updated on {getDayAndMouth(repo.updated_at)}</C.Span>
+
+                  </C.Detail>
+              }
+
+            </C.Details>
+
+          </C.Container>
+        ))
+      }
+    </Carousel>
+  );
 };
 
-export default Carousel;
+export default CarouselDisplay;
--- src/components/Display/Carousel/styles.tsx
+++ src/components/Display/Carousel/styles.tsx
@@ -1,5 +1,108 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  margin-top: 25px;
+  padding: 10px;
+  text-align: center;
+`;
 
-export const Div = styled.div``;
+export const Div = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const DivTitle = styled(Div)`
+  flex-direction: column;
+  padding-bottom: 25px;
+`;
+
+export const Title = styled.span`
+  font-size: 18px;
+  font-weight: 600;
+  color: ${(props) => props.theme.colors.link};
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+export const Visibility = styled.span`
+  color: ${(props) => props.theme.colors.secundary};
+  border-bottom: 1px solid ${(props) => props.theme.colors.tertiary};
+  font-size: 12px;
+  padding: 2px 6px;
+  text-transform: capitalize;
+`;
+
+export const Descrition = styled.span`
+  font-size: 14px;
+  color: ${(props) => props.theme.colors.secundary};
+`;
+
+export const DivLanguage = styled(Div)`
+  padding: 15px 0px;
+`;
+
+export const Language = styled.span`
+  width: 12px;
+  height: 12px;
+  border: 1px solid gray;
+  border-radius: 50%;
+  background-color: ${(props) => props.languageColor};
+  margin-right: 5px;
+`;
+
+export const Image = styled.img`
+  margin-right: 5px;
+`;
+
+export const Span = styled.span``;
+
+export const DivTopics = styled(Div)`
+  flex-wrap: wrap;
+`;
+
+export const Topics = styled.div`
+  font-size: 12px;
+  margin: 2px;
+  padding: 4px 8px;
+  border-radius: 2em;
+  border: 1px solid transparent;
+  cursor: pointer;
+  color: ${(props) => props.theme.colors.link};
+  background: ${(props) => props.theme.background.link};
+  &:hover {
+    color: #fff;
+    background: ${(props) => props.theme.background.linkHover};
+  }
+`;
+
+export const Details = styled(Div)`
+  color: ${(props) => props.theme.colors.secundary};
+  justify-content: space-evenly;
+  padding-top: 15px;
+`;
+
+export const Detail = styled(Div)`
+  font-size: 12px;
+  padding: 5px 0px;
+`;
+
+export const DetailFork = styled(Detail)`
+  cursor: pointer;
+  &:hover {
+    color: ${(props) => props.theme.colors.link};
+  }
+`;
+
+export const DetailStar = styled(DetailFork)`
+  cursor: pointer;
+  &:hover {
+    color: ${(props) => props.theme.colors.link};
+  }
+`;
