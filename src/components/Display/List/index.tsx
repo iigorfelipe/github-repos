c1a8b563760a27c363a690727882c239991c81a9
Igor <iigorfelipe@gmail.com>
2022-10-02 23:00:31 -0300

    ✨ feat: implements List component

--- src/components/Display/List/index.tsx
+++ src/components/Display/List/index.tsx
@@ -1,7 +1,124 @@
+/* eslint-disable prettier/prettier */
+import { useContext } from 'react';
+import { ThemeContext } from 'styled-components';
+import getDayAndMouth from '../../../helpers/convertDate';
+
+import languages from '../../../helpers/languageColors.json';
+import GithubContext from '../../../providers/GithubContext';
+
 import * as C from './styles';
 
 const List = () => {
-  return <C.Container>LISTA</C.Container>;
+  const { title } = useContext(ThemeContext);
+
+  const { reposFiltered } = useContext(GithubContext);
+
+  return (
+    <C.Ol>
+      {
+        reposFiltered.map((repo) => (
+          <C.Li key={repo.id}>
+
+            <C.Div>
+
+              <C.Div2>
+
+                <C.Title>{repo.name}</C.Title>
+
+                <C.Visibility>{repo.visibility}</C.Visibility>
+
+              </C.Div2>
+
+              <C.Div2>
+
+                <C.Descrition>
+                  {
+                    repo.description
+                      ? repo.description
+                      : 'Descrição vazia.'
+                  }
+                </C.Descrition>
+
+              </C.Div2>
+
+                {
+                  repo.topics?.length
+                    ? repo.topics?.length > 0 &&
+                      <C.DivTopics>
+
+                        {          
+                          repo.topics?.map((topic) => (
+                            <C.Topics key={topic}>{topic}</C.Topics>
+                          ))
+                        }
+
+                      </C.DivTopics>
+                    : ''
+                }
+
+              <C.Div2>
+                
+                <C.Details>
+                  {
+                    repo.language &&
+                      <C.Detail>
+
+                        <C.Language languageColor={languages[repo.language]} />
+
+                        <C.Span>{repo.language}</C.Span>
+
+                      </C.Detail>
+                  }
+
+                  {
+                    repo.stargazers_count > 0 &&
+                      <C.Detail>
+
+                        <C.Image src={`./star-${title}.svg`} />
+
+                        <C.Span>{repo.stargazers_count}</C.Span>
+
+                      </C.Detail>
+                  }
+
+                  {
+                    repo.forks >= 1 &&
+                      <C.Detail>
+
+                        <C.Image src={`./repo-forked-16-${title}.svg`} />
+
+                        <C.Span>{repo.forks}</C.Span>
+
+                      </C.Detail>
+                  }
+
+                  {
+                    repo.license && 
+                      <C.Detail>
+                        <C.Image src={`./balance-scale-${title}.svg`} />
+
+                        <C.Span>{repo.license.name}</C.Span>
+
+                      </C.Detail>
+                  }
+
+                  {
+                    repo.updated_at && 
+                      <C.Detail>
+
+                        <C.Span>Updated on {getDayAndMouth(repo.updated_at)}</C.Span>
+
+                      </C.Detail>
+                  }
+
+                </C.Details>
+              </C.Div2>
+            </C.Div>
+          </C.Li>
+        ))
+      }
+    </C.Ol>
+  );
 };
 
 export default List;
--- src/components/Display/List/styles.tsx
+++ src/components/Display/List/styles.tsx
@@ -1,5 +1,113 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Ol = styled.ol`
+  list-style: none;
+  margin-top: 8px;
+`;
 
-export const Div = styled.div``;
+export const Li = styled.li`
+  width: 896px;
+  max-height: 200px;
+  padding: 24px 0px;
+  border-top: 1px solid ${(props) => props.theme.colors.tertiary};
+`;
+
+export const Div = styled.div`
+  width: 672x;
+  height: 100%;
+`;
+
+export const Div2 = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+export const Title = styled.span`
+  font-size: 20px;
+  font-weight: 600;
+  color: ${(props) => props.theme.colors.link};
+  margin-right: 0px;
+  cursor: pointer;
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+export const Visibility = styled.span`
+  color: ${(props) => props.theme.colors.secundary};
+  border: 1px solid ${(props) => props.theme.colors.tertiary};
+  font-size: 12px;
+  margin-left: 8px;
+  padding: 2px 7px;
+  border-radius: 2em;
+  text-transform: capitalize;
+`;
+
+export const Descrition = styled.span`
+  margin-top: 9px;
+  font-size: 14px;
+  color: ${(props) => props.theme.colors.secundary};
+`;
+
+export const DivTopics = styled(Div2)`
+  padding: 12px 0px;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+export const Topics = styled.div`
+  font-size: 12px;
+  margin-right: 2px;
+  padding: 4px 8px;
+  border-radius: 2em;
+  border: 1px solid transparent;
+  cursor: pointer;
+  color: ${(props) => props.theme.colors.link};
+  background: ${(props) => props.theme.background.link};
+  &:hover {
+    color: #fff;
+    background: ${(props) => props.theme.background.linkHover};
+  }
+`;
+
+export const Details = styled.div`
+  width: 406px;
+  height: 18px;
+  font-size: 12px;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  color: ${(props) => props.theme.colors.secundary};
+  margin-top: 15px;
+`;
+
+export const Detail = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  margin-right: 15px;
+`;
+
+export const DivForks = styled(Detail)`
+  &:hover {
+    cursor: pointer;
+    color: ${(props) => props.theme.colors.link};
+  }
+`;
+
+export const Language = styled.span`
+  width: 12px;
+  height: 12px;
+  border: 1px solid gray;
+  border-radius: 50%;
+  background-color: ${(props) => props.languageColor};
+  margin-right: 5px;
+`;
+
+export const Image = styled.img`
+  margin-right: 5px;
+`;
+
+export const Span = styled.span``;
